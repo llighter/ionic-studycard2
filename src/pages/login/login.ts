e454b646085a3ed5be5fda8e1f9eb175ea35c4d5
Park, Yunha <hadbsgk@gmail.com>
2018-04-03 01:21:14 +0900

    add login page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AuthServiceProvider} from "../../providers/auth-service/auth-service";
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController
+              , public navParams: NavParams
+              , private _auth: AuthServiceProvider) {
+  }
+
+  login() {
+    // TODO: 로그인이 되어있지 않은 경우와 그렇지 않은 경우 구분하기
+    this._auth.login();
+  }
+
+  loginWithFacebook() {
+  }
+
+  logout() {
+    this._auth.logout();
+  }
+
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
