63833b3a040e6a9b81628f2dd3e2490d69c47b22
Park, Yunha <hadbsgk@gmail.com>
2018-04-26 02:16:43 +0900

    delete interface for modularization

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "angularfire2/auth";
-import {AngularFirestore, AngularFirestoreDocument} from "angularfire2/firestore"
+import { AngularFirestore, AngularFirestoreDocument } from "angularfire2/firestore"
 import { Observable } from "rxjs/Observable";
 
 import * as firebase from 'firebase/app';
 import 'rxjs/add/operator/switchMap';
+import { User } from '../../domain/user';
 
-interface User {
-  uid: string;
-  email: string;
-  photoURL?: string;
-  displayName?: string;
-}
 
 /*
   Generated class for the AuthServiceProvider provider.
