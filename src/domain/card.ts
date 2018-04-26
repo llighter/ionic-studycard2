fe554ebaeabd449732436af7bf735c5b621ac6ed
Park, Yunha <hadbsgk@gmail.com>
2018-04-27 03:32:55 +0900

    Add cardID property to Card domain

--- src/domain/card.ts
+++ src/domain/card.ts
@@ -2,6 +2,7 @@
 // SQL 테이블에 insert할 때 기본값을 주는 것 처럼 NoSQL도 가능한가?
 // 그런데 firestore가 기본적으로 schemaless 이지 않나?
 interface Card {
+    cardID: string;
     question: string;
     answer: string;
     source: string;
